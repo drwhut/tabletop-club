ac3e86293e7065ebc0f7d42cc365572f8bfdaee6
drwhut <drwhut@gmail.com>
2020-06-29 10:03:11 +0100

    Added ability to create a D6.

---
+++ game/GameUI.gd
@@ -0,0 +1,50 @@
+# OpenTabletop
+# Copyright (c) 2020 drwhut
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+extends Control
+
+signal piece_requested()
+
+onready var _objectsDialog = $ObjectsDialog
+onready var _objectsTree = $ObjectsDialog/ObjectsTree
+
+func _ready():
+	var root = _objectsTree.create_item()
+	_objectsTree.set_hide_root(true)
+	
+	var ott_node = _objectsTree.create_item(root)
+	ott_node.set_text(0, "OpenTabletop")
+	
+	var dice_node = _objectsTree.create_item(ott_node)
+	dice_node.set_text(0, "Dice")
+	
+	var d6_node = _objectsTree.create_item(dice_node)
+	d6_node.set_text(0, "D6")
+
+func _on_ObjectsButton_pressed():
+	_objectsDialog.popup_centered()
+
+func _on_ObjectsTree_item_activated():
+	var selected = _objectsTree.get_selected()
+	
+	# Check the selected item has no children.
+	if not selected.get_children():
+		emit_signal("piece_requested")
---
+++ game/Room.gd
@@ -0,0 +1,34 @@
+# OpenTabletop
+# Copyright (c) 2020 drwhut
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+extends Spatial
+
+const PIECE_SPAWN_HEIGHT = 1.0
+
+onready var _pieces = $Pieces
+
+func add_piece():
+	var d6 = load("res://Die.tscn").instance()
+	
+	# Spawn the piece at a height.
+	d6.translation.y = PIECE_SPAWN_HEIGHT
+	
+	_pieces.add_child(d6)
