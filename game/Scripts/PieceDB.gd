11415f5b1d0869919862273503fee06c43bd6bcf
drwhut <drwhut@gmail.com>
2020-07-02 22:46:32 +0100

    Switched to using open_tabletop_import_module.

--- game/Scripts/PieceDB.gd
+++ game/Scripts/PieceDB.gd
@@ -21,42 +21,57 @@
 
 extends Node
 
-const VALID_TEXTURE_EXTENSIONS = ["png"]
+const ASSET_DIR_PATHS = ["./assets", "../assets"]
+
+# List taken from:
+# https://docs.godotengine.org/en/3.2/getting_started/workflow/assets/importing_images.html
+const VALID_TEXTURE_EXTENSIONS = ["bmp", "dds", "exr", "hdr", "jpeg", "jpg",
+	"png", "tga", "svg", "svgz", "webp"]
 
 # NOTE: Pieces are stored similarly to the directory structures, but all piece
 # types are direct children of the game, i.e. "OpenTabletop/dice/d6" in the
 # game directory is _db["OpenTabletop"]["d6"] here.
 var _db = {}
 
+# From the open_tabletop_import_module:
+# https://github.com/drwhut/open_tabletop_import_module
+var _importer = TabletopImporter.new()
+
 func get_db() -> Dictionary:
 	return _db
 
 func import_all() -> void:
-	import_game_dir("res://OpenTabletop")
-	
-	# TODO: Check directories in the user:// directory!
-
-func import_game_dir(dir_path: String) -> void:
 	var dir = Directory.new()
 	
-	if dir.open(dir_path) == OK:
-		
-		var game_name = dir.get_current_dir().get_file()
-		
-		_db[game_name] = {}
-		
-		if dir.dir_exists("dice"):
-			dir.change_dir("dice")
+	for asset_dir in ASSET_DIR_PATHS:
+		if dir.open(asset_dir) == OK:
+			dir.list_dir_begin(true, true)
 			
-			_add_dir_if_exists(dir, game_name, "d4", "res://Pieces/Dice/d4.tscn")
-			_add_dir_if_exists(dir, game_name, "d6", "res://Pieces/Dice/d6.tscn")
-			_add_dir_if_exists(dir, game_name, "d8", "res://Pieces/Dice/d8.tscn")
-			
-			dir.change_dir("..")
-	else:
-		push_error("Cannot scan " + dir_path + " to import assets!")
+			var entry = dir.get_next()
+			while entry:
+				
+				if dir.current_is_dir():
+					dir.change_dir(entry)
+					import_game_dir(dir)
+					dir.change_dir("..")
+				
+				entry = dir.get_next()
 
-func _add_dir_if_exists(current_dir: Directory, game_name: String, dir: String,
+func import_game_dir(dir: Directory) -> void:
+	var game_name = dir.get_current_dir().get_file()
+	
+	_db[game_name] = {}
+	
+	if dir.dir_exists("dice"):
+		dir.change_dir("dice")
+		
+		_import_dir_if_exists(dir, game_name, "d4", "res://Pieces/Dice/d4.tscn")
+		_import_dir_if_exists(dir, game_name, "d6", "res://Pieces/Dice/d6.tscn")
+		_import_dir_if_exists(dir, game_name, "d8", "res://Pieces/Dice/d8.tscn")
+		
+		dir.change_dir("..")
+
+func _import_dir_if_exists(current_dir: Directory, game_name: String, dir: String,
 	model: String) -> void:
 	
 	if current_dir.dir_exists(dir):
@@ -69,7 +84,14 @@ func _add_dir_if_exists(current_dir: Directory, game_name: String, dir: String,
 		var file = current_dir.get_next()
 		while file:
 			if VALID_TEXTURE_EXTENSIONS.has(file.get_extension()):
-				_add_entry_to_array(array, current_dir, file, model)
+				var texture_path = current_dir.get_current_dir() + "/" + file
+				
+				var error = _importer.import_texture(texture_path, game_name, dir)
+				
+				if error == OK:
+					_add_entry_to_array(array, game_name, dir, file, model)
+				else:
+					print("Failed to import ", texture_path)
 				
 			file = current_dir.get_next()
 		
@@ -77,10 +99,12 @@ func _add_dir_if_exists(current_dir: Directory, game_name: String, dir: String,
 		
 		current_dir.change_dir("..")
 
-func _add_entry_to_array(array: Array, dir: Directory, file: String,
+func _add_entry_to_array(array: Array, game: String, type: String, file: String,
 	model_path: String) -> void:
 	
 	var name = file.substr(0, file.length() - file.get_extension().length() - 1)
-	var texture_path = dir.get_current_dir() + "/" + file
+	
+	# This is where the texture should be copied to with the TabletopImporter.
+	var texture_path = "user://" + game + "/" + type + "/" + file
 	
 	array.push_back(PieceDBEntry.new(name, model_path, texture_path))
