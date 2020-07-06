7bf5175736784c1b65c40cb62d0f03b17596a2f1
drwhut <drwhut@gmail.com>
2020-07-06 10:31:59 +0100

    Added the StackablePiece.
    This will be used as a base class for pieces like cards and chips,
    which can be stacked. Currently, it is only able to detect when it
    should be stacked, it doesn't actually stack yet.

--- game/Scripts/StackablePiece.gd
+++ game/Scripts/StackablePiece.gd
@@ -19,7 +19,36 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-extends StackablePiece
+extends Piece
+
+class_name StackablePiece
+
+const DISTANCE_THRESHOLD = 1.0
+const DOT_STACK_THRESHOLD = 0.9
 
 func _ready():
-	_mesh_instance = $MeshInstance
+	connect("body_entered", self, "_on_body_entered")
+
+func _can_stack(body) -> bool:
+	var me = transform
+	var you = body.transform
+	
+	if (me.origin - you.origin).length() < DISTANCE_THRESHOLD:
+		if abs(me.basis.y.dot(you.basis.y)) > DOT_STACK_THRESHOLD:
+			if abs(me.basis.z.dot(you.basis.z)) > DOT_STACK_THRESHOLD:
+				return true
+	
+	return false
+
+func _on_body_entered(body):
+	if get_tree().is_network_server():
+		
+		# We can't write "if body is StackablePiece", since according to Godot
+		# that would cause a cyclic reference. So instead, we'll check to see
+		# if it is a generic Piece, then check if the model used for the piece
+		# is the same as ours, since we only want to stack items of the same
+		# shape.
+		if body is Piece:
+			if body.piece_entry.model_path == piece_entry.model_path:
+				if _can_stack(body):
+					print("Stacking!")
