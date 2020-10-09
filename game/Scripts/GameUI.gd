3de138a5123218d88fbe2b000f000817d0562565
drwhut <drwhut@gmail.com>
2020-10-09 01:17:57 +0100

    Objects menu has been revisited to include previews of the pieces.
    However, it is not yet possible to add pieces with the new menu,
    and it has not yet been optimised.

--- game/Scripts/GameUI.gd
+++ game/Scripts/GameUI.gd
@@ -27,24 +27,27 @@ signal rotation_amount_updated(rotation_amount)
 
 onready var _game_menu_background = $GameMenuBackground
 onready var _objects_dialog = $ObjectsDialog
-onready var _objects_tree = $ObjectsDialog/ObjectsTree
+onready var _objects_packs = $ObjectsDialog/HBoxContainer/VBoxContainer/ScrollContainer/Packs
+onready var _objects_content = $ObjectsDialog/HBoxContainer/ScrollContainer/Content
+onready var _objects_content_container = $ObjectsDialog/HBoxContainer/ScrollContainer
 onready var _options_menu = $OptionsMenu
 onready var _player_list = $PlayerList
 onready var _rotation_option = $TopPanel/RotationOption
+
+var _piece_db = {}
+var _preview_width = 0
+var _toggled_pack = ""
 
 # Apply options from the options menu.
 # config: The options to apply.
 func apply_options(config: ConfigFile) -> void:
 	pass
 
-# Set the piece tree contents, based on the piece database given.
+# Set the piece database contents, based on the piece database given.
 # pieces: The database from the PieceDB.
-func set_piece_tree_from_db(pieces: Dictionary) -> void:
-	var root = _objects_tree.create_item()
-	_objects_tree.set_hide_root(true)
-	
-	for game in pieces:
-		_add_game_to_tree(game, pieces[game])
+func set_piece_db(pieces: Dictionary) -> void:
+	_piece_db = pieces
+	_update_object_packs()
 
 func _ready():
 	Lobby.connect("player_added", self, "_on_Lobby_player_added")
@@ -53,6 +56,12 @@ func _ready():
 	
 	# Make sure we emit the signal when all of the nodes are ready:
 	call_deferred("_set_rotation_amount")
+	
+	var preview_test = preload("res://Scenes/ObjectPreview.tscn").instance()
+	add_child(preview_test)
+	_preview_width = preview_test.rect_size.x
+	remove_child(preview_test)
+	preview_test.free()
 
 func _unhandled_input(event):
 	if event.is_action_pressed("game_menu"):
@@ -60,71 +69,57 @@ func _unhandled_input(event):
 		if not _game_menu_background.visible:
 			_options_menu.visible = false
 
-# Add a game's piece entries to the piece tree.
-# game_name: The name of the game.
-# game_pieces: The game's piece entries.
-func _add_game_to_tree(game_name: String, game_pieces: Dictionary) -> void:
-	var game_node = _objects_tree.create_item(_objects_tree.get_root())
-	game_node.set_text(0, game_name)
-	game_node.collapsed = true
-	
-	_add_type_to_tree(game_node, game_pieces, ["cards"], "Cards")
-	
-	var dice_node = _objects_tree.create_item(game_node)
-	dice_node.set_text(0, "Dice")
-	
-	_add_type_to_tree(dice_node, game_pieces, ["dice/d4"], "d4")
-	_add_type_to_tree(dice_node, game_pieces, ["dice/d6"], "d6")
-	_add_type_to_tree(dice_node, game_pieces, ["dice/d8"], "d8")
-	
-	if not dice_node.get_children():
-		dice_node.free()
-	
-	_add_type_to_tree(game_node, game_pieces, [
-		"pieces/cube",
-		"pieces/custom",
-		"pieces/cylinder"
-	], "Pieces")
-	
-	_add_type_to_tree(game_node, game_pieces, ["stacks"], "Stacks")
-	
-	_add_type_to_tree(game_node, game_pieces, [
-		"tokens/cube",
-		"tokens/cylinder"
-	], "Tokens")
+# Add an object to a control node.
+# parent: The control node to add the object to.
+# piece_entry: The entry representing the piece.
+func _add_content_object(parent: Control, piece_entry: Dictionary) -> void:
+	var preview = preload("res://Scenes/ObjectPreview.tscn").instance()
+	parent.add_child(preview)
+	preview.set_piece(piece_entry)
 
-# Add a piece to the piece tree.
-# parent: The parent node of the piece's node in the tree.
-# piece: The piece entry of the piece to add.
-func _add_piece_to_tree(parent: TreeItem, piece: Dictionary) -> TreeItem:
-	var node = _objects_tree.create_item(parent)
-	node.set_text(0, piece["name"])
+# Add a set of objects of a given type to a control node, if it exists in the
+# piece database.
+# parent: The control node to add the content to.
+# pack_name: The name of the pack to get the objects from.
+# type_names: The names of the types in the database.
+# type_display: What the type should be called in the UI.
+func _add_content_type(parent: Control, pack_name: String, type_names: Array,
+	type_display: String) -> void:
 	
-	# Keep the piece entry in the node so we can use it later.
-	node.set_metadata(0, piece)
+	if not _piece_db.has(pack_name):
+		return
 	
-	return node
-
-# Add a type of piece to the piece tree.
-# parent: The parent node to add the type node to.
-# game_pieces: The game's piece entries.
-# type_names: The names of the type in the dictionary.
-# display_name: The name of the type in the piece tree.
-func _add_type_to_tree(parent: TreeItem, game_pieces: Dictionary,
-	type_names: Array, display_name: String) -> void:
+	var type_found = false
+	for type_name in type_names:
+		if _piece_db[pack_name].has(type_name):
+			type_found = true
+			break
+	if not type_found:
+		return
 	
-	var node = _objects_tree.create_item(parent)
-	node.set_text(0, display_name)
-	node.collapsed = true
+	var type_label = Label.new()
+	type_label.text = type_display
+	parent.add_child(type_label)
+	
+	var grid_container = GridContainer.new()
+	grid_container.columns = _get_num_content_columns()
+	parent.add_child(grid_container)
 	
 	for type_name in type_names:
-		if game_pieces.has(type_name):
-			var array: Array = game_pieces[type_name]
-			for piece in array:
-				_add_piece_to_tree(node, piece)
+		if _piece_db[pack_name].has(type_name):
+			var piece_entries: Array = _piece_db[pack_name][type_name]
+			for piece_entry in piece_entries:
+				_add_content_object(grid_container, piece_entry)
 	
-	if not node.get_children():
-		node.free()
+	var blank_space = Control.new()
+	blank_space.rect_min_size = Vector2(0, 10)
+	parent.add_child(blank_space)
+
+# Get the number of columns the content grid containers should have.
+func _get_num_content_columns() -> int:
+	if _preview_width < 1:
+		return 1
+	return int(_objects_content_container.rect_size.x / _preview_width)
 
 # Call to emit a signal for the camera to set it's piece rotation amount.
 func _set_rotation_amount() -> void:
@@ -133,6 +128,49 @@ func _set_rotation_amount() -> void:
 		var deg_text = _rotation_option.get_item_text(deg_id)
 		var rad = deg2rad(float(deg_text))
 		emit_signal("rotation_amount_updated", rad)
+
+# Update the content list in the objects menu.
+# pack_name: The name of the pack whose content to display. If the pack doesn't
+# exist, then nothing is displayed.
+func _update_object_content(pack_name: String) -> void:
+	for child in _objects_content.get_children():
+		_objects_content.remove_child(child)
+		child.queue_free()
+	
+	_add_content_type(_objects_content, pack_name, ["cards"], "Cards")
+	
+	_add_content_type(_objects_content, pack_name, ["dice/d4"], "Dice - D4")
+	_add_content_type(_objects_content, pack_name, ["dice/d6"], "Dice - D6")
+	_add_content_type(_objects_content, pack_name, ["dice/d8"], "Dice - D8")
+	
+	_add_content_type(_objects_content, pack_name, [
+		"pieces/cube",
+		"pieces/custom",
+		"pieces/cylinder"
+		], "Pieces")
+	
+	_add_content_type(_objects_content, pack_name, ["stacks"], "Stacks")
+	
+	_add_content_type(_objects_content, pack_name, [
+		"tokens/cube",
+		"tokens/cylinder"
+		], "Tokens")
+
+# Update the pack list in the objects menu.
+func _update_object_packs() -> void:
+	for child in _objects_packs.get_children():
+		_objects_packs.remove_child(child)
+		child.queue_free()
+	
+	for pack_name in _piece_db:
+		var pack_button = Button.new()
+		pack_button.size_flags_horizontal = SIZE_EXPAND_FILL
+		pack_button.text = pack_name
+		pack_button.toggle_mode = true
+		
+		pack_button.connect("toggled", self, "_on_pack_button_toggled")
+		
+		_objects_packs.add_child(pack_button)
 
 # Update the player list based on what is in the Lobby.
 func _update_player_list() -> void:
@@ -168,18 +206,33 @@ func _on_MainMenuButton_pressed():
 func _on_ObjectsButton_pressed():
 	_objects_dialog.popup_centered()
 
-func _on_ObjectsTree_item_activated():
-	var selected = _objects_tree.get_selected()
-	
-	# Check the selected item has metadata.
-	if selected.get_metadata(0):
-		emit_signal("piece_requested", selected.get_metadata(0))
-
 func _on_OptionsButton_pressed():
 	_options_menu.visible = true
 
+func _on_ObjectsDialog_popup_hide():
+	for child in _objects_packs.get_children():
+		child.pressed = false
+	_toggled_pack = ""
+	
+	_update_object_content("")
+
 func _on_OptionsMenu_applying_options(config: ConfigFile):
 	emit_signal("applying_options", config)
+
+func _on_pack_button_toggled(pressed: bool):
+	if pressed:
+		var _new_pack = ""
+		for pack_button in _objects_packs.get_children():
+			if pack_button.pressed:
+				if pack_button.text == _toggled_pack:
+					pack_button.pressed = false
+				else:
+					_new_pack = pack_button.text
+		_toggled_pack = _new_pack
+	else:
+		_toggled_pack = ""
+		
+	_update_object_content(_toggled_pack)
 
 func _on_RotationOption_item_selected(index: int):
 	_set_rotation_amount()
---
+++ game/Scripts/ObjectPreview.gd
@@ -0,0 +1,83 @@
+# open-tabletop
+# Copyright (c) 2020 Benjamin 'drwhut' Beddows
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+extends Control
+
+onready var _camera = $CenterContainer/ViewportContainer/Viewport/Camera
+onready var _label = $Label
+onready var _viewport = $CenterContainer/ViewportContainer/Viewport
+
+const REVOLUTIONS_PER_SECOND = 0.25
+const X_ROTATION = PI / 4
+
+var _piece: Piece = null
+
+# Set the preview to display the given piece.
+# piece_entry: The entry of the piece to display.
+func set_piece(piece_entry: Dictionary) -> void:
+	_label.text = piece_entry["name"]
+	
+	if piece_entry.has("texture_paths"):
+		_piece = preload("res://Pieces/Stack.tscn").instance()
+	else:
+		# This is a piece.
+		_piece = PieceBuilder.build_piece(piece_entry)
+	
+	# Adjust the angle so we can see the top face.
+	_piece.rotate_object_local(Vector3.RIGHT, X_ROTATION)
+	
+	_piece.mode = RigidBody.MODE_STATIC
+	
+	_viewport.add_child(_piece)
+	
+	if _piece is Stack:
+		PieceBuilder.fill_stack(_piece, piece_entry)
+	
+	# Adjust the camera's position so it can see the entire piece.
+	var scale = piece_entry["scale"]
+	var piece_height = scale.y
+	
+	if _piece is Card:
+		piece_height = 0
+	elif _piece is Stack:
+		var test_piece = load(piece_entry["scene_path"]).instance()
+		var is_card_stack = test_piece is Card
+		test_piece.free()
+		if is_card_stack:
+			piece_height = 0
+		else:
+			piece_height *= piece_entry["texture_paths"].size()
+	
+	var piece_radius = max(scale.x, scale.z) / 2
+	
+	var x_cos = cos(X_ROTATION)
+	var x_sin = sin(X_ROTATION)
+	var display_height = 2 * piece_radius * x_sin + piece_height * x_cos
+	var display_radius = piece_radius * x_cos + 0.5 * piece_height * x_sin
+	
+	var theta = deg2rad(_camera.fov)
+	var dist = 1 + display_radius + (display_height / (2 * tan(theta / 2)))
+	_camera.translation.z = dist
+
+func _process(delta):
+	if _piece:
+		var delta_theta = 2 * PI * REVOLUTIONS_PER_SECOND * delta
+		_piece.rotate_object_local(Vector3.UP, delta_theta)
