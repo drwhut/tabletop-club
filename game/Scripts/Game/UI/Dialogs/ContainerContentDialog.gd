07b9306fb7f6a46e56ae37799620d1a2ae3ca60e
drwhut <drwhut@gmail.com>
2021-02-23 02:23:15 +0000

    Made a custom control that displays objects in a grid.
    The container content dialog now uses this new control.
    
    This is in preparation for revisiting how objects are displayed in
    the objects dialog.

--- game/Scripts/Game/UI/Dialogs/ContainerContentDialog.gd
+++ game/Scripts/Game/UI/Dialogs/ContainerContentDialog.gd
@@ -24,99 +24,45 @@ extends WindowDialog
 signal take_all_from(container)
 signal take_from(container, names)
 
-onready var _empty_label = $VBoxContainer/EmptyLabel
-onready var _preview_container = $VBoxContainer/PreviewGridContainer
-
-onready var _far_left_button = $VBoxContainer/HBoxContainer/FarLeftButton
-onready var _left_button = $VBoxContainer/HBoxContainer/LeftButton
-onready var _page_label = $VBoxContainer/HBoxContainer/PageLabel
-onready var _right_button = $VBoxContainer/HBoxContainer/RightButton
-onready var _far_right_button = $VBoxContainer/HBoxContainer/FarRightButton
-
-onready var _take_button = $VBoxContainer/HBoxContainer2/TakeButton
-onready var _take_all_button = $VBoxContainer/HBoxContainer2/TakeAllButton
+onready var _preview_grid = $VBoxContainer/ObjectPreviewGrid
+onready var _take_button = $VBoxContainer/HBoxContainer/TakeButton
+onready var _take_all_button = $VBoxContainer/HBoxContainer/TakeAllButton
 
 var _last_container: PieceContainer = null
-var _last_page: int = 0
 
 # Display the contents of the given container.
 # container: The container to display the contents of.
-# page: The page number to display.
-func display_contents(container: PieceContainer, page: int = 0):
-	page = int(max(0, page))
+func display_contents(container: PieceContainer) -> void:
+	_last_container = container
 	
 	var container_name = container.piece_entry["name"]
 	window_title = "Contents of %s" % container_name
 	
-	_empty_label.visible = false
-	_preview_container.visible = false
-	
-	_far_left_button.disabled = true
-	_left_button.disabled = true
-	_right_button.disabled = true
-	_far_right_button.disabled = true
-	
 	_take_button.disabled = true
 	_take_all_button.disabled = true
 	
-	var container_count = container.get_piece_count()
-	var page_size = _preview_container.get_child_count()
-	var last_page = max(0, (container_count - 1) / page_size)
-	page = int(min(page, last_page))
-	_page_label.text = "Page %d/%d" % [page+1, last_page+1]
-	
-	if page > 0:
-		_far_left_button.disabled = false
-		_left_button.disabled = false
-	if page < last_page:
-		_right_button.disabled = false
-		_far_right_button.disabled = false
-	
-	if container_count == 0:
-		_empty_label.text = "%s is empty." % container_name
-		_empty_label.visible = true
-	else:
-		var piece_names = container.get_piece_names()
-		for i in range(page_size):
-			var preview: ObjectPreview = _preview_container.get_child(i)
-			var item_index = (page * page_size) + i
-			if item_index < container_count:
-				var duplicate = container.duplicate_piece(piece_names[item_index])
-				preview.set_piece(duplicate)
-			else:
-				preview.clear_piece()
-		
-		_preview_container.visible = true
-		_take_all_button.disabled = false
-	
-	# Make sure none of the previews look selected now that we've generated a
-	# new set of them.
-	get_tree().call_group("preview_selected", "set_selected", false)
-	
-	_last_container = container
-	_last_page = page
+	_preview_grid.empty_text = "%s is empty." % container_name
+	_preview_grid.reset()
 
-func _ready():
-	for child in _preview_container.get_children():
-		if child is ObjectPreview:
-			child.connect("clicked", self, "_on_preview_clicked")
-
-func _on_FarLeftButton_pressed():
-	display_contents(_last_container, 0)
-
-func _on_FarRightButton_pressed():
-	# Assume there will never be less than one piece preview per page.
-	display_contents(_last_container, _last_container.get_piece_count())
-
-func _on_LeftButton_pressed():
-	display_contents(_last_container, _last_page - 1)
-
-func _on_RightButton_pressed():
-	display_contents(_last_container, _last_page + 1)
-
-func _on_preview_clicked(_preview: ObjectPreview, _event: InputEventMouseButton):
+func _on_ObjectPreviewGrid_preview_clicked(_preview: ObjectPreview, _event: InputEventMouseButton):
 	var none_selected = get_tree().get_nodes_in_group("preview_selected").empty()
 	_take_button.disabled = none_selected
+
+func _on_ObjectPreviewGrid_requesting_objects(start: int, length: int):
+	var objects = []
+	
+	var piece_names = _last_container.get_piece_names()
+	for i in range(start, start+length):
+		if i >= piece_names.size():
+			break
+		
+		var duplicate = _last_container.duplicate_piece(piece_names[i])
+		objects.append(duplicate)
+	
+	_take_all_button.disabled = objects.empty()
+	
+	var after = max(0, piece_names.size() - start - length)
+	_preview_grid.provide_objects(objects, after)
 
 func _on_TakeAllButton_pressed():
 	emit_signal("take_all_from", _last_container)
---
+++ game/Scripts/Game/UI/Previews/ObjectPreviewGrid.gd
@@ -0,0 +1,239 @@
+# open-tabletop
+# Copyright (c) 2020-2021 Benjamin 'drwhut' Beddows
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# The reason this is an editor script is so that we can adjust the number of
+# rows and columns in the container and be able to immediately see the result
+# in the editor.
+tool
+extends VBoxContainer
+
+class_name ObjectPreviewGrid
+
+signal preview_clicked(preview, event)
+signal requesting_objects(start, length)
+
+export(int, 1, 10) var columns: int = 1 setget set_columns, get_columns
+export(int, 1, 10) var rows: int = 1 setget set_rows, get_rows
+export(String) var empty_text: String = "" setget set_empty_text, get_empty_text
+
+var _page_index: int = 0
+var _page_last: int = 0
+var _start: int = 0
+
+var _empty_label: Label
+var _preview_container: GridContainer
+
+var _first_button: Button
+var _previous_button: Button
+var _page_label: Label
+var _next_button: Button
+var _last_button: Button
+
+var _object_preview = preload("res://Scenes/Game/UI/Previews/ObjectPreview.tscn")
+
+# Get the number of columns in the grid.
+# Returns: The number of columns being displayed.
+func get_columns() -> int:
+	return _preview_container.columns
+
+# Get the text that is displayed when there a no objects being shown.
+# Returns: The text shown when there are no objects.
+func get_empty_text() -> String:
+	return _empty_label.text
+
+# Get the number of previews in the grid.
+# Returns: The number of previews being displayed.
+func get_preview_count() -> int:
+	return get_columns() * get_rows()
+
+# Get the number of rows in the grid.
+# Returns: The number of rows being displayed.
+func get_rows() -> int:
+	return rows
+
+# Provide the preview grid with the set of objects it requested.
+# objects: The array of objects the grid wanted. It can be made up of both
+# orphan piece nodes and piece entries. If empty, the empty text is displayed.
+# after: The number of pieces that should come after this array. This is used
+# to calculate the total number of pages.
+func provide_objects(objects: Array, after: int) -> void:
+	if Engine.editor_hint:
+		return
+	
+	_empty_label.visible = objects.empty()
+	_preview_container.visible = not objects.empty()
+	
+	for i in range(get_preview_count()):
+		var preview: ObjectPreview = _preview_container.get_child(i)
+		if i < objects.size():
+			var piece = objects[i]
+			if piece is Piece:
+				if not piece.is_inside_tree():
+					preview.set_piece(piece)
+				else:
+					push_error("Piece %d in object array is not an orphan node!" % i)
+			elif piece is Dictionary:
+				preview.set_piece_with_entry(piece)
+			else:
+				push_error("Element %d in object array is not a piece or piece entry!" % i)
+		else:
+			preview.clear_piece()
+	
+	_page_index = _start / get_preview_count()
+	_page_last = _page_index + int(ceil(floor(after) / get_preview_count()))
+	
+	_page_label.text = "Page %d/%d" % [_page_index+1, _page_last+1]
+	
+	_first_button.disabled = true
+	_previous_button.disabled = true
+	_next_button.disabled = true
+	_last_button.disabled = true
+	
+	if _page_index > 0:
+		_first_button.disabled = false
+		_previous_button.disabled = false
+	if _page_index < _page_last:
+		_next_button.disabled = false
+		_last_button.disabled = false
+
+# Reset the preview grid. This should be called when it is about to be
+# displayed.
+func reset() -> void:
+	if Engine.editor_hint:
+		return
+	
+	# Make sure none of the previews look selected.
+	get_tree().call_group("preview_selected", "set_selected", false)
+	
+	_request_objects(0)
+
+# Set the number of columns in the grid.
+# num_cols: The number of columns to display.
+func set_columns(num_cols: int) -> void:
+	assert(num_cols > 0)
+	_preview_container.columns = num_cols
+	
+	_setup_previews()
+
+# Set the text that is displayed when there a no objects being shown.
+# text: The text shown when there are no objects.
+func set_empty_text(text: String) -> void:
+	_empty_label.text = text
+
+# Set the number of rows in the grid.
+# num_rows: The number of rows to display.
+func set_rows(num_rows: int) -> void:
+	assert(num_rows > 0)
+	rows = num_rows
+	
+	_setup_previews()
+
+func _init():
+	# If we're in the editor, make sure there are no children when we start.
+	for child in get_children():
+		remove_child(child)
+		child.queue_free()
+	
+	_empty_label = Label.new()
+	_empty_label.align = Label.ALIGN_CENTER
+	_empty_label.valign = Label.VALIGN_CENTER
+	_empty_label.size_flags_horizontal = SIZE_EXPAND_FILL
+	_empty_label.size_flags_vertical = SIZE_EXPAND_FILL
+	add_child(_empty_label)
+	
+	_preview_container = GridContainer.new()
+	_preview_container.visible = false
+	_preview_container.size_flags_horizontal = SIZE_EXPAND_FILL
+	_preview_container.size_flags_vertical = SIZE_EXPAND_FILL
+	add_child(_preview_container)
+	
+	var hbox = HBoxContainer.new()
+	hbox.size_flags_horizontal = SIZE_EXPAND_FILL
+	add_child(hbox)
+	
+	_first_button = Button.new()
+	_first_button.text = "<<"
+	_first_button.connect("pressed", self, "_on_first_button_pressed")
+	hbox.add_child(_first_button)
+	
+	_previous_button = Button.new()
+	_previous_button.text = "<"
+	_previous_button.connect("pressed", self, "_on_previous_button_pressed")
+	hbox.add_child(_previous_button)
+	
+	_page_label = Label.new()
+	_page_label.align = Label.ALIGN_CENTER
+	_page_label.size_flags_horizontal = SIZE_EXPAND_FILL
+	hbox.add_child(_page_label)
+	
+	_next_button = Button.new()
+	_next_button.text = ">"
+	_next_button.connect("pressed", self, "_on_next_button_pressed")
+	hbox.add_child(_next_button)
+	
+	_last_button = Button.new()
+	_last_button.text = ">>"
+	_last_button.connect("pressed", self, "_on_last_button_pressed")
+	hbox.add_child(_last_button)
+
+# Request objects from a given start index.
+# start: The start index to request.
+func _request_objects(start: int) -> void:
+	assert(start >= 0)
+	
+	_start = start
+	emit_signal("requesting_objects", _start, get_preview_count())
+
+# Setup the previews in the grid container.
+func _setup_previews() -> void:
+	var current_children = _preview_container.get_child_count()
+	var target_children = get_preview_count()
+	
+	while current_children < target_children:
+		var preview = _object_preview.instance()
+		preview.connect("clicked", self, "_on_preview_clicked")
+		_preview_container.add_child(preview)
+
+		current_children += 1
+	
+	while current_children > target_children:
+		var child = _preview_container.get_child(current_children - 1)
+		_preview_container.remove_child(child)
+		child.queue_free()
+		
+		current_children -= 1
+
+func _on_first_button_pressed():
+	_request_objects(0)
+
+func _on_previous_button_pressed():
+	_request_objects(max(0, _start - get_preview_count()))
+
+func _on_next_button_pressed():
+	var max_start = _page_last * get_preview_count()
+	_request_objects(min(_start + get_preview_count(), max_start))
+
+func _on_last_button_pressed():
+	_request_objects(_page_last * get_preview_count())
+
+func _on_preview_clicked(preview: ObjectPreview, event: InputEventMouseButton):
+	# Forward the signal outside the grid.
+	emit_signal("preview_clicked", preview, event)
