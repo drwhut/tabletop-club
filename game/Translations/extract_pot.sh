d857d7bd83855307a37855c8c4fa8aefa47c3d48
drwhut <drwhut@gmail.com>
2021-03-05 09:24:14 +0000

    Created translation template file for the game.

---
+++ game/Translations/extract_pot.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install the Python packages.
+python3 -m pip install -r requirements.txt
+
+# Extract the .pot file.
+pybabel extract -F babelrc.txt -k text -k LineEdit/placeholder_text -k tr \
+    -k load_button_text -k status_text_one -k status_text_multiple -o godot-l10n.pot ..
