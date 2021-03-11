8928f018e7527e081e2e3c7df6cccd50e36bcb1c
drwhut <drwhut@gmail.com>
2021-03-11 08:31:27 +0000

    Skyboxes can now be labeled as 'default'.
    If a skybox has been labeled as such, it will be loaded before the
    game starts.
    
    Note that this only works for skyboxes in the default asset pack.
    It could potentially work for other asset packs in the future when
    the main menu "theming" is implemented.

--- game/Scripts/AssetDB.gd
+++ game/Scripts/AssetDB.gd
@@ -508,7 +508,9 @@ func _import_asset(from: String, pack: String, type_dir: String,
 			}
 	elif type_asset == ASSET_SKYBOX:
 		if VALID_TEXTURE_EXTENSIONS.has(to.get_extension()):
+			var default = _get_file_config_value(config, from.get_file(), "default", false)
 			entry = {
+				"default": default,
 				"description": desc,
 				"name": _get_file_without_ext(to),
 				"texture_path": to
