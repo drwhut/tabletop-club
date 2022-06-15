031fdd6b531bf2a8e4f51bdaf9bb57c86bb0e75e
drwhut <drwhut@gmail.com>
2022-06-15 13:54:27 +0100

    Added tooltips to certain controls in the UI.
    This helps players understand what certain controls do, for example,
    what the anti-aliasing option does in the video settings, and how it
    affects gameplay.
    
    This commit fixes #21.

--- game/Translations/extract_pot.sh
+++ game/Translations/extract_pot.sh
@@ -5,4 +5,5 @@ python3 -m pip install -r requirements.txt
 
 # Extract the .pot file.
 pybabel extract -F babelrc.txt -k text -k LineEdit/placeholder_text -k tr \
-    -k load_button_text -k status_text_one -k status_text_multiple -o godot-l10n.pot ..
+    -k load_button_text -k status_text_one -k status_text_multiple -k hint_tooltip \
+    -o godot-l10n.pot ..
