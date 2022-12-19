9acea7cbf5a2921263f891981aab3091ae561e54
elmodor <elmodor@users.noreply.github.com>
2022-12-19 11:34:00 +0100

    Force Vector3 for scale for all objects (except cards)

--- game/Scripts/AssetDB.gd
+++ game/Scripts/AssetDB.gd
@@ -827,8 +827,10 @@ func _import_asset(from: String, pack: String, type: String, config: ConfigFile,
 	# We usually deal with the config values at the end, but some assets need
 	# these values for the entry initialization.
 	var scale = _get_file_config_value(config, from.get_file(), "scale", Vector3.ONE)
-	if type == "cards" or typeof(scale) == TYPE_VECTOR2:
+	if type == "cards":
 			scale = Vector3(scale.x, 1, scale.y)
+	elif typeof(scale) != TYPE_VECTOR3:
+		push_error("Scale for type %s has to be Vector3!" % type)
 	
 	var entry = {}
 	if asset_type == ASSET_AUDIO:
