9a94bc18fea130750b4256a265acfadb1e744722
drwhut <drwhut@gmail.com>
2023-09-12 09:27:11 +0100

    Moved the SplashScreen scene to res://main/.

---
+++ game/main/splash_screen.gd
@@ -0,0 +1,49 @@
+# tabletop-club
+# Copyright (c) 2020-2023 Benjamin 'drwhut' Beddows.
+# Copyright (c) 2021-2023 Tabletop Club contributors (see game/CREDITS.tres).
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+extends AnimationPlayer
+
+## The splash screen for the game, which shows the Godot Engine logo.
+
+
+func _ready():
+	connect("animation_finished", self, "_on_animation_finished")
+	
+	# Load the default asset pack as register it as early as possible.
+	var ttc_pack := preload("res://assets/default_pack/ttc_pack.tres")
+	AssetDB.add_pack(ttc_pack)
+	AssetDB.commit_changes()
+	
+	# TODO: Check 'language' option, set locale.
+	# TODO: Check 'skip_splash_screen' option.
+	play("FadeInAndOut")
+
+
+func _unhandled_input(event: InputEvent):
+	if event is InputEventJoypadButton or \
+		event is InputEventScreenTouch or \
+		event is InputEventKey:
+			pass
+
+
+func _on_animation_finished(_anim_name: String):
+	pass
