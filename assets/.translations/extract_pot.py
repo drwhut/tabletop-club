e028e669dea414039e62bbcd79f815a2b6c3ec75
drwhut <drwhut@gmail.com>
2024-01-06 18:19:39 +0000

    Updated the copyright notice to include the year 2024.

--- assets/.translations/extract_pot.py
+++ assets/.translations/extract_pot.py
@@ -10,10 +10,10 @@ import polib
 import sys
 
 HEADER = """Tabletop Club Default Asset Pack Translation Template.
-Copyright (c) 2020-2023 Benjamin 'drwhut' Beddows.
-Copyright (c) 2021-2023 Tabletop Club contributors (see game/CREDITS.tres).
+Copyright (c) 2020-2024 Benjamin 'drwhut' Beddows.
+Copyright (c) 2021-2024 Tabletop Club contributors (see game/CREDITS.tres).
 This file is distributed under the same license as the Tabletop Club package.
-Benjamin 'drwhut' Beddows <drwhut@gmail.com>, 2023.
+Benjamin 'drwhut' Beddows <drwhut@gmail.com>, 2024.
 """
 
 if len(sys.argv) < 2:
